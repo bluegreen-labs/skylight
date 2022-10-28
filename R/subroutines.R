51967e5f2b24118c5786f4a68ba73957ff92fcf6
Koen Hufkens <koen.hufkens@gmail.com>
2022-10-28 22:37:00 +0200

    floating points

--- R/subroutines.R
+++ R/subroutines.R
@@ -6,10 +6,10 @@
 
 sun <- function(D, DR, RD, CE, SE){
   T <- 280.46 + 0.98565 * D
-  T <- T - as.integer(T/360.0) * 360.0
+  T <- T - as.integer(T/360) * 360
 
   # vectorize using ifelse statement
-  T <- ifelse(T < 0.0, T + 360.0, T)
+  T <- ifelse(T < 0, T + 360, T)
 
   G <- (357.5 + 0.98560 * D) * DR
   LS <- (T + 1.91 * sin(G)) * DR
@@ -17,11 +17,11 @@ sun <- function(D, DR, RD, CE, SE){
   Y <- cos(LS)
 
   # vectorize using ifelse statement
-  AS <- ifelse(Y < 0.0, AS + 180.0, AS)
+  AS <- ifelse(Y < 0, AS + 180, AS)
 
   SD <- SE * sin(LS)
   DS <- asin(SD)
-  T <- T - 180.0
+  T <- T - 180
 
   return(
     data.frame(
@@ -43,10 +43,10 @@ sun <- function(D, DR, RD, CE, SE){
 
 moon <- function(D, G, CE, SE, RD, DR){
   V <- 218.32 + 13.1764 * D
-  V <- V - as.integer(V/360.0) * 360.0
+  V <- V - as.integer(V/360) * 360
 
   # vectorize using ifelse statement
-  V <- ifelse(V < 0.0, V + 360.0, V)
+  V <- ifelse(V < 0, V + 360, V)
 
   Y <- (134.96 + 13.06499 * D) * DR
   O <- (93.27 + 13.22935 * D) * DR
@@ -70,7 +70,7 @@ moon <- function(D, G, CE, SE, RD, DR){
   AS <- atan(P/Q) * RD
 
   # vectorize using ifelse statement
-  AS <- ifelse(Q < 0.0, AS + 180.0, AS)
+  AS <- ifelse(Q < 0, AS + 180, AS)
 
   return(
     data.frame(
@@ -98,10 +98,10 @@ altaz <- function(DS, H, SD, CI, SI, DR, RD){
   AZ <- atan(P/Q) * RD
 
   # vectorize using ifelse statement
-  AZ <- ifelse(Q < 0.0, AZ + 180.0, AZ)
+  AZ <- ifelse(Q < 0, AZ + 180, AZ)
 
   # vectorize using ifelse statement
-  AZ <- ifelse(AZ < 0.0, AZ + 360.0, AZ)
+  AZ <- ifelse(AZ < 0, AZ + 360, AZ)
 
   AZ <- as.integer(AZ + 0.5)
   H <- asin(SD * SI + CD * CI * CS) * RD
@@ -119,9 +119,9 @@ altaz <- function(DS, H, SD, CI, SI, DR, RD){
 
 refr <- function(H,DR){
   # vectorize using ifelse statement
-  HA <- ifelse(H < (-5.0/6.0),
+  HA <- ifelse(H < (-5/6),
               H,
-              H+1.0/(tan((H+8.6/(H+4.42))*DR))/60.0
+              H+1/(tan((H+8.6/(H+4.42))*DR))/60
   )
   return(HA)
 }
@@ -135,9 +135,9 @@ refr <- function(H,DR){
 atmos <- function(HA,DR){
   U <- sin(HA * DR)
   X <- 753.66156
-  S <- asin(X * cos(HA * DR)/(X + 1.0))
+  S <- asin(X * cos(HA * DR)/(X + 1))
   M <- X * (cos(S) - U) + cos(S)
   M <- exp(-0.21 * M) * U + 0.0289 * exp(-0.042 * M) *
-    (1.0 + (HA + 90.0) * U/57.29577951)
+    (1 + (HA + 90) * U/57.29577951)
   return(M)
 }
