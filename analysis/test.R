a6356348dcf8df41c9a2f42cee527de9287959ed
KH <koen.hufkens@gmail.com>
2024-11-16 18:41:57 +0100

    updated test routines

--- analysis/test.R
+++ analysis/test.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-#  R CMD INSTALL --preclean --no-multiarch --with-keep.source skylight
+#R CMD INSTALL --preclean --no-multiarch --with-keep.source skylight
 
 # load the library
 library(skylight)
@@ -15,44 +15,56 @@ input <- data.frame(
   sky_condition = 1
 )
 
-microbenchmark(
-  "R" = {skylight(
-    input
-  )},
-  "Fortran" = {skylight(
-    input,
-    fast = TRUE
-  )}
-)
+input_quick <- input[1:20,]
 
 # calculate sky illuminance values for
 # a single date/time and location
-df <- skylight(
-  input
+df1 <- skylight(
+  input_quick
 ) |>
   select(
     (starts_with("sun") | starts_with("moon"))
   )
 
-print(head(df))
+print(head(df1))
 
 # calculate sky illuminance values for
 # a single date/time and location
-df <- skylight(
-  input,
+df2 <- skylight(
+  input_quick,
   fast = TRUE
 ) |>
   select(
     (starts_with("sun") | starts_with("moon"))
   )
 
-print(head(df))
+print(head(df2))
 
-library(profvis)
-
-profvis::profvis({
-  skylight(
+b <- microbenchmark(
+  "R" = {skylight(
+    input
+  )},
+  "Fortran" = {skylight(
     input,
     fast = TRUE
-  )
-})
+  )},
+  times = 100
+)
+
+par(mfrow = c(3,1))
+boxplot(b)
+
+R <- b$time[b$expr == "R"]
+hist(log(R))
+
+F <- b$time[b$expr != "R"]
+hist(log(F))
+
+# library(profvis)
+#
+# profvis::profvis({
+#   skylight(
+#     input,
+#     fast = TRUE
+#   )
+# })
