b179af746d4d2d74f24cef97997f091e4d483606
Koen Hufkens <koen.hufkens@gmail.com>
2025-08-05 21:11:59 +0200

    benchmark

--- analysis/test.R
+++ analysis/test.R
@@ -1,70 +1,26 @@
-#!/usr/bin/env Rscript
-
-#R CMD INSTALL --preclean --no-multiarch --with-keep.source skylight
-
-# load the library
+library(tidyverse)
 library(skylight)
-library(tidyr)
-library(dplyr)
 library(microbenchmark)
 
-input <- data.frame(
-  longitude = 0,
-  latitude = 50,
-  date =  as.POSIXct("2020-06-18 00:00:00", tz = "GMT") + seq(0, 60*24*3600, 900),
-  sky_condition = 1
-)
+df <- readRDS("../the_long_journey_of_CC894/data/geolocator_data_annotated.rds")
 
-input_quick <- input[1:20,]
-
-# calculate sky illuminance values for
-# a single date/time and location
-df1 <- skylight(
-  input_quick
-) |>
-  select(
-    (starts_with("sun") | starts_with("moon"))
-  )
-
-print(head(df1))
-
-# calculate sky illuminance values for
-# a single date/time and location
-df2 <- skylight(
-  input_quick,
-  fast = TRUE
-) |>
-  select(
-    (starts_with("sun") | starts_with("moon"))
-  )
-
-print(head(df2))
-
-b <- microbenchmark(
-  "R" = {skylight(
-    input
-  )},
-  "Fortran" = {skylight(
-    input,
+results <- microbenchmark(
+  skylight(
+    longitude = 3.72875,
+    latitude = 51.0779,
+    date = df$date_time[1:24],
+    sky_condition = 1,
     fast = TRUE
-  )},
-  times = 100
+  ),
+  skylight(
+    longitude = 3.72875,
+    latitude = 51.0779,
+    date = df$date_time[1:24],
+    sky_condition = 1,
+    fast = FALSE
+  ),
+  times = 10000
 )
 
-par(mfrow = c(3,1))
-boxplot(b)
-
-R <- b$time[b$expr == "R"]
-hist(log(R))
-
-F <- b$time[b$expr != "R"]
-hist(log(F))
-
-# library(profvis)
-#
-# profvis::profvis({
-#   skylight(
-#     input,
-#     fast = TRUE
-#   )
-# })
+print(results)
+boxplot(results, names=c('fast', 'slow'))
