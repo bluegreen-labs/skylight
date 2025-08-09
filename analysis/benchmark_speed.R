6a0bd72e1d5911e77ffe6c8cadf528ae0f4339e0
Koen Hufkens <koen.hufkens@gmail.com>
2025-08-09 16:09:58 +0200

    speed test

--- analysis/benchmark_speed.R
+++ analysis/benchmark_speed.R
@@ -8,18 +8,40 @@ results <- microbenchmark(
   skylight(
     longitude = 3.72875,
     latitude = 51.0779,
-    date = df$date_time[1:24],
+    date = df$date_time[1:100],
     sky_condition = 1,
     fast = TRUE
   ),
   skylight(
     longitude = 3.72875,
     latitude = 51.0779,
-    date = df$date_time[1:24],
+    date = df$date_time[1:100],
     sky_condition = 1,
     fast = FALSE
   ),
-  times = 10000
+  times = 100
+)
+
+print(results)
+boxplot(results, names=c('fast', 'slow'))
+
+
+results <- microbenchmark(
+  skylight(
+    longitude = 3.72875,
+    latitude = 51.0779,
+    date = df$date_time[1:10000],
+    sky_condition = 1,
+    fast = TRUE
+  ),
+  skylight(
+    longitude = 3.72875,
+    latitude = 51.0779,
+    date = df$date_time[1:10000],
+    sky_condition = 1,
+    fast = FALSE
+  ),
+  times = 100
 )
 
 print(results)
